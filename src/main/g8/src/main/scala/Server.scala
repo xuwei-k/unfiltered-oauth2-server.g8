af11986039d29e6760866be8a52a6ac1021fe886
kenji yoshida <6b656e6a69@gmail.com>
2017-07-09 07:48:33 +0900

    fix procedure syntax

--- src/main/g8/src/main/scala/Server.scala
+++ src/main/g8/src/main/scala/Server.scala
@@ -5,7 +5,7 @@ import java.net.URL
 
 object Server {
 
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     val binding = SocketPortBinding(host = "localhost", port = $port$)
     unfiltered.jetty.Server.portBinding(binding)
       .resources(new URL(getClass.getResource("/web/robots.txt"), "."))
